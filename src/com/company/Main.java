6dadc4ba87e8636e6171ec8b4a46d36c647e84b7
Sandi-san <nicksrebotsandi@gmail.com>
2021-03-16 13:18:05 +0100

    Select in insert stavek Klicanje baze iz tujega class-a

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,25 @@
 package com.company;
 
+import java.sql.*;
+import java.util.Properties;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Database db = new Database();
+
+        try {
+            db.Vnos("Skem", 9999);
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        try {
+            db.Izpis();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+
     }
 }
