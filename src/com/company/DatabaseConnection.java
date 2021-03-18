5e125c7e234f8afd82e8aeb930e78ebd16f97bf8
dejan966 <59951914+dejan966@users.noreply.github.com>
2021-03-18 15:12:11 +0100

    Funkcija za povezavo do baze, Insert za admine in studente

--- src/com/company/DatabaseConnection.java
+++ src/com/company/DatabaseConnection.java
@@ -6,58 +6,86 @@ import java.sql.*;
 public class DatabaseConnection {
     public static void main(String[] args) {
 
-
     }
 
     private Connection conn;
-    String jdbcURL = "jdbc:postgresql://ec2-52-213-167-210.eu-west-1.compute.amazonaws.com:5432/d72om3lphmskj1";
-    String username = "agzigsarirffns";
-    String password = "0bbb063c271cbf8b6ad1108669ae17fe0e978ab530c693b62fdc6b4debae87ca";
 
-    /*public Connection getConnection() throws SQLException {
+    public Connection Connect() throws SQLException {
         try {
             String jdbcURL = "jdbc:postgresql://ec2-52-213-167-210.eu-west-1.compute.amazonaws.com:5432/d72om3lphmskj1";
             String username = "agzigsarirffns";
             String password = "0bbb063c271cbf8b6ad1108669ae17fe0e978ab530c693b62fdc6b4debae87ca";
             //Class.forName("com.mysql.jdbc.Driver").newInstance();
-            if (conn == null) {
-                conn = DriverManager.getConnection(jdbcURL, username, password);
-            }
-
+            conn = DriverManager.getConnection(jdbcURL, username, password);
 
         }
         catch (SQLException throwables) {
             throwables.printStackTrace();
         }
         return conn;
-    }*/
+    }
 
 
     public void Save(String ime_k, int posta) throws SQLException {
-        conn = DriverManager.getConnection(jdbcURL, username, password);
-        Statement stmt = conn.createStatement();
+        try(Connection connection = Connect()){
+            Statement stmt = conn.createStatement();
 
-        String sql = "INSERT INTO kraji(ime, post_st) VALUES ('" + ime_k + "', '" + posta + "')";
-        stmt.executeUpdate(sql);
+            String sql = "INSERT INTO kraji(ime, post_st) VALUES ('" + ime_k + "', '" + posta + "')";
+            stmt.executeUpdate(sql);
+            conn.close();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
 
-        conn.close();
     }
 
     public void Izpis() throws SQLException{
-        conn = DriverManager.getConnection(jdbcURL, username, password);
-        Statement stmt = conn.createStatement();
+        try(Connection connection = Connect()){
+            Statement stmt = conn.createStatement();
 
-        String sql = "SELECT * FROM kraji";
-        ResultSet rs = stmt.executeQuery(sql);
-        while(rs.next()) {
-            int id = rs.getInt("id");
-            String ime = rs.getString("ime");
-            int post_st = rs.getInt("post_st");
-            //Display values
-            System.out.print("ID: " + id);
-            System.out.print(", Ime: " + ime);
-            System.out.println(", Poštna št.: " + post_st);
+            String sql = "SELECT * FROM kraji";
+            ResultSet rs = stmt.executeQuery(sql);
+            while(rs.next()) {
+                int id = rs.getInt("id");
+                String ime = rs.getString("ime");
+                int post_st = rs.getInt("post_st");
+
+                System.out.print("ID: " + id);
+                System.out.print(", Ime: " + ime);
+                System.out.println(", Poštna št.: " + post_st);
+            }
+            conn.close();
         }
-        conn.close();
+        catch(SQLException e){
+            e.printStackTrace();
+        }
     }
+
+    public void SignUp(String name, String surname, String gender, String d, int number, String u, String mail, String Upass, String kraj){
+        try(Connection connection = Connect()){
+            Statement stmt = conn.createStatement();
+
+            String sql = "INSERT INTO studenti(ime, priimek, spol, datum_roj, telefon, username, email, pass, kraj_id) VALUES ('" + name + "', '" + surname + "' , '" + gender + "'  , '" + d + "', '" + number + "', '" + u + "', '" + mail + "', '" + Upass + "', (SELECT ime FROM kraji WHERE ime = '" + kraj +"'))";
+            stmt.executeUpdate(sql);
+            conn.close();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void AdminSignIn(String mail, String passs) throws SQLException{
+        try(Connection connection = Connect()){
+            Statement stmt = conn.createStatement();
+
+            String sql = "INSERT INTO admini(username, pass) VALUES ('" + mail + "', '" + passs + "')";
+            stmt.executeUpdate(sql);
+            conn.close();
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+
 }
--- src/com/company/prijava.java
+++ src/com/company/prijava.java
@@ -1,9 +1,29 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class prijava {
-    private JButton button1;
+    DatabaseConnection dc = new DatabaseConnection();
+
+    private JButton Btn_SignIn;
     private JTextField textField1;
     private JTextField textField2;
+
+    public prijava() {
+        Btn_SignIn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                String email = textField1.getText();
+                String geslo = textField2.getText();
+                JOptionPane.showMessageDialog(null, "Hello world");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        //JFrame frame = new JFrame("Prijava");
+
+    }
 }
