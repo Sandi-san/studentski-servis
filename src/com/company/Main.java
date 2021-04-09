eceb61c493c5bdee7800aee67d420eadeb2c59dd
dejan966 <59951914+dejan966@users.noreply.github.com>
2021-04-09 10:50:38 +0200

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
+        //testMerge
         //System.out.println("Hello world");
         DatabaseConnection dc = new DatabaseConnection();
         String z = "bla bla";
