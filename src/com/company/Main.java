98645b04b415f961de4080c81dfbdd6dab1b80f2
dejan966 <59951914+dejan966@users.noreply.github.com>
2021-04-12 15:17:55 +0200

    admin registracija in prijava v masterju

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,21 +3,6 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        //testMerge
-        //System.out.println("Hello world");
-        DatabaseConnection dc = new DatabaseConnection();
-        String z = "bla bla";
-        int u = 2000;
-        try {
-            dc.Save(z,u);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
-        try {
-            dc.Izpis();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        new registracija();
     }
 }
